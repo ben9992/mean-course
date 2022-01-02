dbed973775036ac0c93b987f8c3656d6a2690de5
Ben Mishali <Ben.Mishali@elbitsystems.com>
2022-01-02 20:02:15 +0200

    added routes

--- backend/app.js
+++ backend/app.js
@@ -2,9 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-
-const Post = require('./models/post');
-
+const postsRoutes = require("./routes/posts")
 const app = express();
 
 mongoose
@@ -32,56 +30,5 @@ app.use((req, res, next) => {
   next();
 })
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save().then(createdPost => {
-    res.status(201).json({
-      message: "Post added successfully",
-      postId: createdPost._id
-    });
-  })
-})
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-
-  Post.updateOne({ _id: req.params.id }, post).then(result => {
-    res.status(201).json({
-      message: "Post updated successfully"
-    });
-  })
-})
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then(docs => {
-    res.status(200).json(docs);
-  })
-})
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post)
-      res.status(200).json(post);
-    else
-      res.status(404).json({
-        message: "Post did not found"
-      });
-  })
-})
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id }).then(() => {
-    res.status(200).json({
-      message: "Post deleted successfully"
-    });
-  })
-})
-
+app.use("/api/posts", postsRoutes)
 module.exports = app;
